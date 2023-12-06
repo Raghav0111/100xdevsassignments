9973a57d180bf7333a1136cb814ea039589e4623
Raghav Kaushal <raghavkaushal@Raghavs-MacBook-Air.local>
2023-12-06 09:17:41 +0530

    Anagram and FindLargest Element

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,30 @@
 */
 
 function isAnagram(str1, str2) {
+  let n1 = str1.length;
+  let n2 = str2.length;
+
+  if(n1 != n2){
+    return false;
+  }
+
+str1 = str1.toLowerCase();
+str2 = str2.toLowerCase();
+
+let charArray = str1.split('');
+let charArray2 = str2.split('');
+
+charArray.sort();
+charArray2.sort();
+
+for(let i=0;i<n1;i++){
+  if(charArray[i]!== charArray2[i]){
+    return false;
+  }
+}
+  return true;
 
 }
+
 
 module.exports = isAnagram;
